ed13c4b2d78d021bcbadfd8d7558d1c4c2fd0c12
王寅鵬 <yinpeng.wang@renren-inc.com>
2015-09-11 18:26:31 +0800

    ListView Item 图片移动

--- ReadPoetry/src/main/java/com/cpp/readpoetry/adapter/ImageListAdapter.java
+++ ReadPoetry/src/main/java/com/cpp/readpoetry/adapter/ImageListAdapter.java
@@ -1,18 +1,14 @@
 package com.cpp.readpoetry.adapter;
 
 import android.content.Context;
-import android.graphics.Bitmap;
 import android.graphics.Matrix;
-import android.graphics.drawable.BitmapDrawable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
 import android.widget.ImageView;
 import android.widget.TextView;
-
 import com.cpp.readpoetry.R;
-import com.cpp.readpoetry.util.DisplayUtil;
 
 /**
  * Created by Three. on 2015/3/26.
@@ -46,43 +42,40 @@ public class ImageListAdapter extends BaseAdapter {
 
         ViewHolder viewHolder;
         if (convertView == null) {
-
             convertView = LayoutInflater.from(this.mContext).inflate(R.layout.list_item_layout, null, false);
+
             ImageView imageView = (ImageView) convertView.findViewById(R.id.image);
             TextView textView = (TextView) convertView.findViewById(R.id.text);
             imageView.setScaleType(ImageView.ScaleType.MATRIX);
-
-            mContext.getResources().getDrawable(R.drawable.pic_1);
-            // hardcode the background image
-            int srcId = 0;
-            if (position % 4 == 0) {
-                srcId = R.drawable.pic_1;
-            } else if (position % 4 == 1) {
-                srcId = R.drawable.pic_2;
-            } else if (position % 4 == 2) {
-                srcId = R.drawable.pic_3;
-            } else {
-                srcId = R.drawable.pic_4;
-            }
-
-//            Bitmap bitmap = DisplayUtil.drawableToBitmap(mContext.getResources().getDrawable(srcId));
-//            int bmpWidth = bitmap.getWidth();
-//            int bmpHeight = bitmap.getHeight();
-
-            imageView.setImageResource(srcId);
 
             // since the image size is set to 400 x 300 we will hardcode the initial translation to the center for new item
             Matrix matrix = imageView.getImageMatrix();
             matrix.postTranslate(0, -100);
             imageView.setImageMatrix(matrix);
-
+            imageView.setScaleType(ImageView.ScaleType.CENTER_CROP);
+            
             // use the viewholder
             viewHolder = new ViewHolder();
             viewHolder.imageView = imageView;
             viewHolder.textView = textView;
             convertView.setTag(viewHolder);
+        } else {
+            viewHolder = (ViewHolder) convertView.getTag();
         }
-        viewHolder = (ViewHolder) convertView.getTag();
+
+        int srcId;
+        if (position % 4 == 0) {
+            srcId = R.drawable.pic_1;
+        } else if (position % 4 == 1) {
+            srcId = R.drawable.pic_2;
+        } else if (position % 4 == 2) {
+            srcId = R.drawable.pic_3;
+        } else {
+            srcId = R.drawable.pic_4;
+        }
+
+        viewHolder.imageView.setImageResource(srcId);
+
         viewHolder.textView.setText("Row " + position);
         return convertView;
     }
--- ReadPoetry/src/main/java/com/cpp/readpoetry/fragment/HotFragment.java
+++ ReadPoetry/src/main/java/com/cpp/readpoetry/fragment/HotFragment.java
@@ -1,33 +1,24 @@
 package com.cpp.readpoetry.fragment;
 
-import android.content.Context;
 import android.graphics.Matrix;
-import android.os.Bundle;
 import android.os.Handler;
-import android.support.annotation.Nullable;
-import android.support.v4.app.Fragment;
 import android.support.v4.widget.SwipeRefreshLayout;
-import android.view.LayoutInflater;
 import android.view.View;
-import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ImageView;
 import com.cpp.readpoetry.R;
 import com.cpp.readpoetry.adapter.ImageListAdapter;
+import com.cpp.readpoetry.fragment.base.BaseFragment;
 import com.cpp.readpoetry.view.ImageListView;
 import com.cpp.readpoetry.view.OnDetectScrollListener;
-
-import static com.cpp.readpoetry.config.GlobalConfig.*;
 
 /**
  * Created by Three. on 2015/3/20.
  */
-public class HotFragment extends Fragment implements SwipeRefreshLayout.OnRefreshListener,
+public class HotFragment extends BaseFragment implements SwipeRefreshLayout.OnRefreshListener,
         OnDetectScrollListener, AdapterView.OnItemClickListener {
 
-    private Context mContext;
-
-    private View mView;
+    private final String TAG = "HotFragment";
 
     private SwipeRefreshLayout swipeLayout;
     private ImageListView imageListView;
@@ -35,28 +26,22 @@ public class HotFragment extends Fragment implements SwipeRefreshLayout.OnRefres
     private Matrix imageMatrix;
 
     @Override
-    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        mView = inflater.inflate(R.layout.fragment_hot_main, container, false);
-
-        mContext = getActivity();
-
-        initView();
-        return mView;
+    protected int onSetContainerViewId() {
+        return R.layout.fragment_hot_main;
     }
 
-    private void initView() {
-
-        swipeLayout = (SwipeRefreshLayout) mView.findViewById(R.id.swipe_refresh);
+    @Override
+    protected void onInitView() {
+        swipeLayout = (SwipeRefreshLayout) containerView.findViewById(R.id.swipe_refresh);
         swipeLayout.setOnRefreshListener(this);
 
         swipeLayout.setColorSchemeResources(android.R.color.holo_red_light, android.R.color.holo_green_light,
                 android.R.color.holo_blue_bright, android.R.color.holo_orange_light);
 
-        imageListView = (ImageListView) mView.findViewById(R.id.list_view);
-        imageListView.setAdapter(new ImageListAdapter(mContext));
+        imageListView = (ImageListView) containerView.findViewById(R.id.list_view);
+        imageListView.setAdapter(new ImageListAdapter(context));
         imageListView.setOnDetectScrollListener(this);
         imageListView.setOnItemClickListener(this);
-
     }
 
     /**
@@ -95,7 +80,7 @@ public class HotFragment extends Fragment implements SwipeRefreshLayout.OnRefres
         for (int i = 0; i < (last - first); i++) {
             ImageView imageView = ((ImageListAdapter.ViewHolder) imageListView.getChildAt(i).getTag()).imageView;
             imageMatrix = imageView.getImageMatrix();
-            imageMatrix.postTranslate(0.5f, 0);
+            imageMatrix.postTranslate(0f, 0.2f);
             imageView.setImageMatrix(imageMatrix);
             imageView.invalidate();
         }
@@ -108,22 +93,10 @@ public class HotFragment extends Fragment implements SwipeRefreshLayout.OnRefres
         for (int i = 0; i < (last - first); i++) {
             ImageView imageView = ((ImageListAdapter.ViewHolder) imageListView.getChildAt(i).getTag()).imageView;
             imageMatrix = imageView.getImageMatrix();
-            imageMatrix.postTranslate(0.5f, 0);
+            imageMatrix.postTranslate(0f, -0.2f);
             imageView.setImageMatrix(imageMatrix);
             imageView.invalidate();
         }
     }
 
-    Handler handler = new Handler() {
-        public void handleMessage(android.os.Message msg) {
-            switch (msg.what) {
-                case REFRESH_OK:
-                    swipeLayout.setRefreshing(false);
-                    break;
-                case REFRESH_FAIL:
-                    break;
-                default:
-            }
-        }
-    };
 }
